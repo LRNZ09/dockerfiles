4a0b2af2977341c0147645f75778cd3219f6b8f3
Lorenzo Pieri <me@lrnz09.me>
2017-11-17 00:03:35 +0100

    Fixed ghost entrypoint call

--- ghost/docker-entrypoint.sh
+++ ghost/docker-entrypoint.sh
@@ -6,4 +6,4 @@ apk add --no-cache jq
 
 jq ".mail.options.auth.pass = \"$(cat /run/secrets/ghost_mailgun_pass)\"" /var/lib/ghost/config.json > /var/lib/ghost/config.production.json
 
-source '/usr/local/bin/docker-entrypoint.sh'
+docker-entrypoint.sh node current/index.js
